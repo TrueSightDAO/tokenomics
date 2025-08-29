196d0d1225b4cf6cc51666fd734a43294a0693b8
Zhiwen Teh <garyjob@Zhiwens-MBP.local>
2025-08-29 12:37:15 -0300

    sandboxing

--- agroverse_qr_code_web_service/github_webhook_handler.py
+++ agroverse_qr_code_web_service/github_webhook_handler.py
@@ -38,8 +38,12 @@ GITHUB_REPOSITORY = 'TrueSightDAO/qr_codes'
 # Use to_upload directory for temporary file storage
 GITHUB_WORKSPACE = os.path.join(os.getcwd(), 'to_upload')
 
-# Google Sheets configuration
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1GE7PUq-UT6x2rBN-Q2ksogbWpgyuh2SaxJyG_uEK6PU/edit?gid=1552160318#gid=1552160318"
+# PRODUCTION: Google Sheets configuration
+# SHEET_URL = "https://docs.google.com/spreadsheets/d/1GE7PUq-UT6x2rBN-Q2ksogbWpgyuh2SaxJyG_uEK6PU/edit?gid=1552160318#gid=1552160318"
+
+# SANDBOX: Google Sheets configuration
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1qSi_-VSj7yiJl0Ak-Q3lch-l4mrH37cEw8EmQwS_6a4/edit?gid=472328231#gid=472328231"
+
 CURRENCIES_SHEET_NAME = "Currencies"
 QR_CODES_SHEET_NAME = "Agroverse QR codes"
 
